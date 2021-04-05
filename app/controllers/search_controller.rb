8bdcde4e36c2bf5bfade29c096ce98d934d49480
Megan Gonzales <megan.campbell26@yahoo.com>
2021-04-05 09:40:01 -0600

    30 min, working on accessing the api data needed

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,13 @@
 class SearchController < ApplicationController
 
   def index
+    conn = Faraday.new('http://last-airbender-api.herokuapp.com')
+    
+    response = conn.get('/api/v1/characters') do |f|
+      f.params['affiliation'] = params[:nation] 
+    end
 
+    json = JSON.parse(response.body, symbolize_names: true) 
+    # require 'pry'; binding.pry
   end
 end
